3fa67dbb90cac8e3dee997c72641ec953cae3faf
SGLeonid <forestwizard36@gmail.com>
2026-08-25 19:13:25 +0300

    Added JDBC user authentication management

--- src/main/resources/db/migration/V3__create_auth_user.sql
+++ src/main/resources/db/migration/V3__create_auth_user.sql
@@ -1,9 +1,14 @@
 
-USE note_db;
+USE PUBLIC;
 
-CREATE TABLE auth_user (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(256) NOT NULL,
+CREATE TABLE IF NOT EXISTS users(
+    username VARCHAR(256) PRIMARY KEY NOT NULL,
     password VARCHAR(256) NOT NULL,
-    role VARCHAR(16) NOT NULL
+    enabled BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS authorities(
+    username VARCHAR(256) NOT NULL,
+    authority VARCHAR(256) NOT NULL,
+    FOREIGN KEY(username) REFERENCES users(username)
 );
--- src/main/resources/db/migration/V4__populate_auth_user.sql
+++ src/main/resources/db/migration/V4__populate_auth_user.sql
@@ -1,4 +1,8 @@
 
-USE note_db;
+USE PUBLIC;
 
-INSERT INTO auth_user(username, password, role) VALUES('user', '{noop}default', 'USER');
+-- User password is set according to task 2.1
+
+INSERT INTO users(username, password, enabled) VALUES('user', '$2a$10$37YFWkHCVD4jP2Hoe6SaseAVFdnh655mElIW8mOIk.jfIJnBJrpD.', true);
+INSERT INTO authorities(username, authority) VALUES('user', 'USER');
+INSERT INTO authorities(username, authority) VALUES('user', 'ADMIN');
